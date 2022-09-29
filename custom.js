95423c2608e649e31dc1a217178380a6340444f3
zeeshanzaman <88842812+zeeshanzaman@users.noreply.github.com>
2022-09-30 01:55:28 +0500

    add inner HTML

--- custom.js
+++ custom.js
@@ -6,5 +6,12 @@ function addNewNote(text = '') {
     const note = document.createElement('div')
     note.classList.add('note')
 
+    note.innerHTML = `
+    <div class="tools">
+            <button class="edit"><i class="fas fa-edit"></i></button>
+            <button class="delete"><i class="fas fa-trash-alt"></i></button>
+        </div>
 
+        <div class="main"></div>
+        <textarea></textarea>`
 }
