be73a16283c4d0fc0d2593ff0e3eecb8e10bd194
zeeshanzaman <88842812+zeeshanzaman@users.noreply.github.com>
2022-09-30 01:54:08 +0500

    create function with the name of addNewNote

--- custom.js
+++ custom.js
@@ -1,3 +1,10 @@
 const addBtn = document.getElementById('add')
 
 addBtn.addEventListener('click', () => addNewNote())
+
+function addNewNote(text = '') {
+    const note = document.createElement('div')
+    note.classList.add('note')
+
+
+}
