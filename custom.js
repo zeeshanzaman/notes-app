5396a686e017c1a037329b79927b16beecfaaba3
zeeshanzaman <88842812+zeeshanzaman@users.noreply.github.com>
2022-10-01 01:29:58 +0500

    modify coding

--- custom.js
+++ custom.js
@@ -1,6 +1,6 @@
 const addBtn = document.getElementById('add')
 
-addBtn.addEventListener('click', () => addNewNote())
+addBtn.addEventListener('click', () => addNewNote(''))
 
 function addNewNote(text = '') {
     const note = document.createElement('div')
@@ -32,20 +32,17 @@ function addNewNote(text = '') {
         textArea.classList.toggle('hidden')
     })
 
-    textArea.addEventListener('input', (e) => {
-        const { value } = e.target
-
-        main.innerHTML = marked(value)
-
-        document.body.appendChild(note)
-    })
+    updateLS()
 
     document.body.appendChild(note)
 }
 
-function updatesLS() {
+function updateLS() {
     const notesText = document.querySelectorAll('textarea')
 
-    const notes =
+    const notes = []
 
+    notesText.forEach(note => notes.push(note.value))
+
+    localStorage.setItem('notes', JSON.stringify(notes))
 }
